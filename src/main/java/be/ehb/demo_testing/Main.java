dbb907723f32a78b4137c4c394a79d1a1ddf0015
david.van.steertegem@gmail.com <PowerUser420>
2021-05-03 15:24:38 +0200

    Initial commit

---
+++ src/main/java/be/ehb/demo_testing/Main.java
@@ -0,0 +1,8 @@
+package be.ehb.demo_testing;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(MathUtil.som(2,3));
+    }
+}
---
+++ src/main/java/be/ehb/demo_testing/MathUtil.java
@@ -0,0 +1,8 @@
+package be.ehb.demo_testing;
+
+public class MathUtil {
+
+    public static double som(double a, double b){
+        return a + b;
+    }
+}
